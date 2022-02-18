1b8196355c6f1bbb5f63a23bd520eae4846845ad
MarianaGiraldo <mgiraldo594@misena.edu.co>
2022-02-18 12:50:53 -0500

    footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -2,36 +2,32 @@
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
-                <h5 style="font-family: MarkingPen; color:#f7941d; font-size: 5vh">Merca aqui</h5>
+                <h5 style="font-family: MarkingPen; color:#f7941d; font-size: 5vh">Dealership App</h5>
                 <ul>
                     <li><a class="grey-text text-lighten-4" href="/productos"><i
-                                class="tiny material-icons" style="color:#f7941d">check</i> Productos</a></li>
-                    <li><a class="grey-text text-lighten-4" href="/usuarios"><i
-                                class="tiny material-icons" style="color:#f7941d">check</i> Vendedores</a></li>
-                    <li><a class="grey-text text-lighten-4" href="/ventas"><i
-                                class="tiny material-icons" style="color:#f7941d">check</i> Ventas</a></li>
+                                class="tiny material-icons" style="color:#f7941d">check</i> Cars</a></li>
                 </ul>
             </div>
             <div class="col l4 offset-l2 s12">
-                <h5 style="font-family: MarkingPen; color:#f7941d; font-size: 5vh">Contáctenos</h5>
+                <h5 style="font-family: MarkingPen; color:#f7941d; font-size: 5vh">Contact us</h5>
                 <ul>
                     <li>
                         <a class="grey-text text-lighten-3" href="#!"><i 
-                                class="tiny material-icons" style="color:#f7941d">home</i>Cra 9 # 11-26 </a>
+                                class="tiny material-icons" style="color:#f7941d">home</i>Cra 9 # 11-26 Bogota, Colombia </a>
                     </li>
                     <li><a class="grey-text text-lighten-3" href="#!"><i
-                                class="tiny material-icons" style="color:#f7941d">settings_cell</i> 319587458</a></li>
+                                class="tiny material-icons" style="color:#f7941d">settings_cell</i>+57 319587458</a></li>
                     <li><a class="grey-text text-lighten-3" href="#!"><i
-                                class="tiny material-icons" style="color:#f7941d">local_phone</i> 8258745</a></li>
+                                class="tiny material-icons" style="color:#f7941d">local_phone</i>+57 060 8258745</a></li>
                     <li><a class="grey-text text-lighten-3" href="#!"><i 
-                                class="tiny material-icons" style="color:#f7941d">email</i>infomercaaqui@gmail.com</a></li>
+                                class="tiny material-icons" style="color:#f7941d">email</i>info@dealership.com</a></li>
                 </ul>
             </div>
         </div>
     </div>
     <div class="footer-copyright" style="background-color: #1d80f7">
         <div class="container">
-            © 2021 Todos los derechos reservados
+            © 2021 Copy Right reserved
         </div>
     </div>
 </footer>
