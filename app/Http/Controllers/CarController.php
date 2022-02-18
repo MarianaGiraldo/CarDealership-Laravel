dd63e33347c5e5bcd95d29db5e0d6d257c4c3824
MarianaGiraldo <mgiraldo594@misena.edu.co>
2022-02-18 00:54:17 -0500

    Car controller

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\UpdateCarRequest;
 
 class CarController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +20,7 @@ class CarController extends Controller
      */
     public function index()
     {
-        //
+        return view('cars.index', ['cars'=>Car::all(), 'bg'=>'bg1.jpg']);
     }
 
     /**
@@ -25,7 +30,7 @@ class CarController extends Controller
      */
     public function create()
     {
-        //
+        return view('cars.create', ['cars'=>Car::all(), 'bg'=>'bg1.jpg']);
     }
 
     /**
@@ -36,29 +41,59 @@ class CarController extends Controller
      */
     public function store(StoreCarRequest $request)
     {
-        //
+        // Validates form fields
+        $validation = $request->validate([
+            'brand'=>['required', 'string', 'max:255'],
+            'model'=>'required',
+            'year'=>['required', 'integer'],
+            'category'=>['required', 'string'],
+            'price'=>['required', 'integer'],
+            'image'=>'required',
+        ]);
+
+        // Creates a new car from the request parameters.
+        $car = new Car();
+        $car ->brand = $request->get('brand');
+        $car ->model = $request->get('model');
+        $car ->year = $request->get('year');
+        $car ->category = $request->get('category');
+        $car ->price = $request->get('price');
+
+        // Process image to store in database
+        $photo = $request->file('image');
+        $filename = time() . '.' . $photo->getClientOriginalExtension();
+        $destino=public_path('imagenes/cars/');
+        $request->image->move($destino, $filename);
+        $car ->imagen = $filename;
+        
+        $car -> save();
+        return redirect('/cars');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Car  $car
+     * @param  Car $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Car $car)
+    public function show($id)
     {
-        //
+        return view('cars.show', [
+            'car'=>Car::findOrFail($id),
+            'cars'=>Car::all(),
+            'bg'=>'bg1.jpg']);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Car  $car
+     * @param  Car $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(Car $car)
+    public function edit($id)
     {
-        //
+        $car = Car::findOrFail($id);
+        return view('cars.edit', ['car'=>$car, 'bg'=>'bg1.jpg']);
     }
 
     /**
@@ -70,17 +105,45 @@ class CarController extends Controller
      */
     public function update(UpdateCarRequest $request, Car $car)
     {
-        //
+        $carUpdt =  Car::find($id);
+        $carUpdt ->brand = $request->get('brand');
+        $carUpdt ->model = $request->get('model');
+        $carUpdt ->year = $request->get('year');
+        $carUpdt ->category = $request->get('category');
+        $carUpdt ->price = $request->get('price');
+
+        // Process image to store in database
+        $photo = $request->file('image');
+        $filename = time() . '.' . $photo->getClientOriginalExtension();
+        $destino=public_path('imagenes/cars/');
+        $request->image->move($destino, $filename);
+        $carUpdt ->imagen = $filename;
+        
+        $carUpdt -> save();
+        return redirect('/cars');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Car  $car
+     * @param  Car $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Car $car)
+    public function destroy($id)
     {
-        //
+        Car::destroy($id);
+        return redirect('/cars');
+    }
+
+    /**
+     * Ask if is sure to delete the car
+     *
+     * @param  Car $id
+     * @return \Illuminate\Http\Response
+     */
+    public function drop($id)
+    {
+        $dropCar = Car::find($id);
+        return view('cars.drop', ['dropCar'=>$dropCar, 'bg'=>'bg1.jpg']);
     }
 }
